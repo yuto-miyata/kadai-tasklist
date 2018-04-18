754867825f885c244d5379e7cdc4648e3566a651
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-04-18 06:15:33 +0000

    make kadai

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return view('tasks.index', ['tasks' => $tasks,]);
+    }
+    
+    public function show($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.show', ['task' => $task,]);
+    }
+    
+    public function create()
+    {
+        $task = new Task;
+        
+        return view('tasks.create', ['task' => $task,]);
+    }
+    
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+    
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.edit', ['task' => $task,]);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+    
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect('/');
+    }
+    
+    
+}
